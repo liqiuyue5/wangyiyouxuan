536b9f6395580463d6c4c612639c8394b7bd6537
LeeWenBo-360 <3436277100@qq.com>
2020-01-09 09:21:07 +0800

    cart

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,22 +1,398 @@
-<template>
-	<view>
-		category
-	</view>
-</template>
-
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
-</script>
-
-<style>
-
-</style>
+ <template>
+ 	<div class="container">
+ 		<div class="search-list">
+ 			<a href="javascript:;">
+ 			<input type="text" value="" placeholder="搜索商品, 共21987款好物" />
+ 			<div class="search iconfont icon-search "></div>
+ 			</a>
+ 		</div>
+ 		<div class="subcate-list"> 
+ 	 
+ 			<view class="scrollwrapper">
+ 			 	<view class="uni-padding-wrap uni-common-mt">
+ 			 		 
+ 			 			<scroll-view class="scroll-view_H" scroll-x="true" @scroll="scroll" scroll-left="120">
+ 			 			  <ul>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 			 						</view>
+ 			 					</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 									<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 											 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 									</view>
+ 								</li>
+ 								<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <a href="http://m.you.163.com/"><span>年货专区</span></a>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span> 
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					 
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 					<li>
+ 			 						<view id="demo1" class="scroll-view-item_H uni-bg-red">
+ 			 								 <span>年货专区</span>
+ 			 						</view>
+ 			 					</li>
+ 			 				</ul>
+ 			 					
+ 			 			 
+ 			 				 
+ 			 			</scroll-view>
+ 			 		</view>
+ 			 </view>
+ 			  
+ 			 
+ 			 
+ 			
+  
+ 			<div class="subCateContainer">
+ 			 <div class="banner">
+ 					<img src="/static/images/banner.webp" class="singleBanner">
+ 			 </div>
+ 			 <div class="cateList">
+ 				 <ul>
+ 					 <li class="cateItem">
+ 						    <div class="imgclass">
+ 								 <image src="../../static/images/shop_list.webp"  class="imgItem" mode="">	 
+ 								 </image>
+ 								</div>
+ 								<div class="textclass">
+ 									<span>精选好物15元起</span>
+ 								</div>
+ 					 </li>
+ 					 <li class="cateItem">
+ 					 		<div class="imgclass">
+ 					 		 <image src="../../static/images/shop_list.webp"  class="imgItem" mode="">	 
+ 					 		 </image>
+ 					 		</div>
+ 					 		<div class="textclass">
+ 					 			<span>精选好物15元起</span>
+ 					 		</div>				   
+ 					 </li>
+ 					 <li class="cateItem">
+ 					 			<div class="imgclass">
+ 					 			 <image src="../../static/images/shop_list.webp"  class="imgItem" mode="">	 
+ 					 			 </image>
+ 					 			</div>
+ 					 			<div class="textclass">
+ 					 				<span>精选好物15元起</span>
+ 					 			</div>			   
+ 					 </li>
+ 					  
+ 						<li class="cateItem">
+ 									<div class="imgclass">
+ 									 <image src="../../static/images/shop_list.webp"  class="imgItem" mode="">	 
+ 									 </image>
+ 									</div>
+ 									<div class="textclass">
+ 										<span>精选好物15元起</span>
+ 									</div>			   
+ 						</li>
+ 						
+ 				 </ul>
+ 			 </div>			
+ 		 </div>
+ 		
+ 		</div>
+ 		<!-- 尾部 -->
+ 		 
+ 		
+ 	</div>
+ </template>
+ 
+ <script>
+ 	export default {
+ 		data() {
+ 			return {
+ 				scrollTop: 0,
+ 				old: {
+ 					scrollTop: 0
+ 				}
+ 			};
+ 		},
+ 		methods: {
+ 			upper: function(e) {
+ 				console.log(e);
+ 			},
+ 			lower: function(e) {
+ 				console.log(e);
+ 			},
+ 			scroll: function(e) {
+ 				console.log(e);
+ 				this.old.scrollTop = e.detail.scrollTop;
+ 			},
+ 			goTop: function(e) {
+ 				this.scrollTop = this.old.scrollTop;
+ 				this.$nextTick(function() {
+ 					this.scrollTop = 0;
+ 				});
+ 				uni.showToast({
+ 					icon: 'none',
+ 					title: '纵向滚动 scrollTop 值已被修改为 0'
+ 				});
+ 			}
+ 		}
+ 	};
+  
+  
+ </script>
+ 
+ <style lang="stylus">
+ 	// 必须紧紧的靠着边 写代码,否则报错
+ 	// 代码不生效,就是格式问题
+ 	// 不能 鼠标右键格式化代码
+ 	// 在任何情况都要注意 rem值得适配
+ 	.search-list
+ 		width 750upx
+ 		height 80upx
+ 		border 1upx solid #eaeaea
+ 		position  fixed
+ 		background-color white
+ 		z-index 5
+ 		input
+ 			width 600upx
+ 			height 60upx
+ 			border 1upx solid #eaeaea
+ 			margin 3upx auto
+ 			line-height 60upx
+ 			text-align center
+ 			border-radius 8upx
+ 			margin-top 10upx
+ 		.search
+ 			margin-top -65upx
+ 			margin-left 10%
+ 		.icon-search	
+ 			font-size 60upx
+ 	.subcate-list
+ 		width 750upx
+ 		height 1044upx
+  
+ 		flex-direction row
+ 		display flex
+ 		/* 换行 */
+ 		flex-wrap nowrap
+ 	/* flex-wrap: nowrap | wrap | wrap-reverse; 换行不换行,反转 */
+ 	/*对齐方式  justify-content: flex-start | flex-end | center | space-between | space-around;*/
+ 		justify-content space-around
+ 		position relative
+ 		
+ 		// 
+ 		.cateavVert
+ 			width 162upx
+ 			height 1044upx
+ 			border 1px solid #eaeaea
+ 		 
+ 			margin-top 15upx 
+ 			ul
+ 				li
+ 					padding-top  33upx
+ 		.scrollwrapper
+ 			position relative
+ 			.scroll-view
+ 		    
+ 				height 80upx			
+ 				overflow hidden
+ 		.subCateContainer
+ 			width 588upx
+ 			height 1044upx
+ 			border 1px solid #eaeaea
+ 			margin-left 25upx
+ 			margin-top 15upx 
+ 			position  fixed
+ 			left  145upx
+ 			top  80upx 
+ 			
+ 	 
+ 			.banner
+ 				position relative
+ 				width 100%
+ 				height 2.56rem
+ 				display table
+ 				margin-bottom 0.42667rem
+ 				background center no-repeat #eee
+ 				background-size cover
+ 				border-radius 4px
+ 			  .singleBanner
+ 					width 95%
+ 					height 200upx
+ 			
+ 					
+ 				.cateList
+ 					width 528upx
+ 					height 170upx
+  
+ 					.cateItem
+ 						width 176upx
+ 						height 170upx	
+ 			 
+ 						float left
+ 						margin-top  15upx
+ 				 
+ 						.imgItem
+ 							width  176upx
+ 							height 170upx
+ 				 
+ 							float left
+ 					.textclass	
+ 						width  176upx  
+ 						height 72upx
+ 						font-size 26upx	
+ 						display: -webkit-box
+ 						text-align center
+ 				 
+ 						-webkit-box-orient horizontal
+ 	
+ 					
+ </style>
+ 
